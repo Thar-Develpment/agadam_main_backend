91dd50751b1882964e1dd4f6c9ec4e1b9f3feef5
Hçkrsùryã <surya07032000@gmail.com>
2026-09-15 21:54:58 +0530

    get image and video

--- controller/basic.js
+++ controller/basic.js
@@ -16,3 +16,31 @@ exports.getBasicInfo = async (req, res) => {
         }
     });
 };
+
+
+exports.getBasicAssets = async (req, res) => {
+
+    let getQuery = `SELECT * FROM am_basic_assets WHERE status = 1`;
+
+    query(getQuery, (err, data) => {
+        if (err) {
+            return res.json({ status: 0, message: "Something went wrong" });
+        } else if (data?.length == 0) {
+            return res.json({ status: 0, message: "No data found" });
+        } else {
+
+            let imageArr = []
+            let videoArr = []
+
+            data.map(e => {
+                if (e.type == 'image') {
+                    imageArr.push(e.url)
+                } else if (e.type == 'video') {
+                    videoArr.push(e.url)
+                }
+            })
+
+            return res.json({ status: 1, image: { data: imageArr, count: imageArr?.length || 0 }, video: { data: videoArr, count: videoArr?.length || 0 } });
+        }
+    });
+};
--- routes/basic.js
+++ routes/basic.js
@@ -4,6 +4,6 @@ const basic = require('../controller/basic')
 
 router.get('/get_basic_info', basic.getBasicInfo)
 
-
+router.get('/get_basic_assets', basic.getBasicAssets)
 
 module.exports = router;
