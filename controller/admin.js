6624d66bdfa78d7c0e5133a44f4a32293db7c158
Hçkrsùryã <surya07032000@gmail.com>
2026-09-18 09:26:51 +0530

    gallery and video update url validation 30 to 100

--- controller/admin.js
+++ controller/admin.js
@@ -337,7 +337,7 @@ exports.updateGallery = async (req, res) => {
     const v = new Validator(reqData, {
         id: "required|numeric",
         category_id: "required|numeric",
-        image_url: "required|string|maxLength:30",
+        image_url: "required|string|maxLength:100",
         status: "required|in:0,1",
     });
 
@@ -499,7 +499,7 @@ exports.updateVideo = async (req, res) => {
 
     const v = new Validator(reqData, {
         id: "required|numeric",
-        video_url: "required|string|maxLength:30",
+        video_url: "required|string|maxLength:100",
         status: "required|in:0,1",
     });
 
