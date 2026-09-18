1e4f7e37a5b8dc70353b590fb14c9e9b19bd6864
Hçkrsùryã <surya07032000@gmail.com>
2026-09-18 22:10:24 +0530

    db change

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const subDomainRouter = require('./routes/sub_domain')
 const adminRouter = require('./routes/admin')
 
 app.get('/', (req, res) => {
-    res.json({ status: 0.5 })
+    res.json({ status: 0.6 })
 })
 
 app.use('/basic', basicRouter)
