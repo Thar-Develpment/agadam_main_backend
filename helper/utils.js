836f9a38f03d4b656d1080b55b448e0844384417
Hçkrsùryã <surya07032000@gmail.com>
2026-09-20 19:23:19 +0530

    payment middleware

--- helper/utils.js
+++ helper/utils.js
@@ -3,30 +3,24 @@ const query = require("../model/db")
 exports.checkPayment = (req, res, next) => {
 
     let subdomain = req?.user?.subdomain;
-    console.log("subdomain jwt: ", subdomain);
 
     let subDomain;
 
     if (req?.body?.shop_name) {
-        console.log('1');
         subDomain = req?.body?.shop_name + '.aadagam.com'
     } else if (req?.body?.subdomain) {
-        console.log('2');
         subDomain = req?.body?.subdomain
     } else if (subdomain) {
-        console.log('3');
         subDomain = subdomain
     }
-
-    console.log("subDomain final: ", subDomain);
 
     let getQuery = `SELECT id,subdomain,status,created_at,payment_at FROM am_register WHERE subdomain = ?`
 
     query(getQuery, [subDomain], (err, data) => {
         if (err) {
-            res.json({ status: 0, message: "Something went wrong!" })
+            return res.json({ status: 0, message: "Something went wrong!" })
         } else if (data?.length == 0) {
-            res.json({ status: 0, message: "Shop not found!" })
+            return res.json({ status: 0, message: "Shop not found!" })
         } else {
 
             let singleData = data[0]
@@ -45,10 +39,12 @@ exports.checkPayment = (req, res, next) => {
 
                 if (diffInDays > 2) {
                     deActivateSubDomain(subDomain)
-                    res.json({ status: 0, message: "Payment pending" })
+                    return res.json({ status: 0, message: "Payment pending" })
                 } else {
                     next()
                 }
+            } else {
+                next()
             }
 
         }
