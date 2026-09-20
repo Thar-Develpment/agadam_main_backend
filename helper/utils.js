e2df020f4aee18d7ccc7d818b481f9ce12de2f4c
Hçkrsùryã <surya07032000@gmail.com>
2026-09-20 15:28:09 +0530

    payment middleware

--- helper/utils.js
+++ helper/utils.js
@@ -25,7 +25,8 @@ exports.checkPayment = (req, res, next) => {
 
             if (!singleData?.payment_at) {
 
-                const createdAt = singleData?.created_at
+                const createdAt = singleData?.created_at?.toString()
+                console.log("createdAt: ", createdAt);
 
                 const createdDate = new Date(createdAt?.replace(" ", "T"));
 
